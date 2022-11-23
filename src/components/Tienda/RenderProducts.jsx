90e6e8bc661daf86503e68ba38a8cc3b82f09982
cesargaleano <87844938+cesargaleano@users.noreply.github.com>
2022-11-23 00:58:25 -0500

    Responsive Fix

--- src/components/Tienda/RenderProducts.jsx
+++ src/components/Tienda/RenderProducts.jsx
@@ -2,18 +2,32 @@ import React from 'react'
 //import { PRODUCTS } from '../../mockups/products'
 import { Product } from './Product';
 import { useSelector } from 'react-redux';
+import styled from 'styled-components';
 
 export const RenderProducts = () => {
   const PRODUCTS = useSelector(state=>state.product.productsList);
   console.log(PRODUCTS);
   return (
-    <div style={{display:'flex', flexDirection:'row', flexWrap: 'wrap' , gap:'20px'}}>
+    <ProductsWrapper style={{}}>
     {
      PRODUCTS.map((product,index)=>(
        <Product key={index} product={product} />
      ))
      
    }
-   </div>
+   </ProductsWrapper>
   )
 }
+
+const ProductsWrapper = styled.div`
+padding:1rem;
+display:flex;
+justify-content:center;
+align-items: center;
+flex-direction:row;
+flex-wrap: wrap;
+column-gap: 1rem;
+row-gap:1rem;
+
+
+`;
